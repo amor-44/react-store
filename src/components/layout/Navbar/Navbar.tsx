63c8add13cc101e61e20ee3e5df2460adf5b31d7
Amr Hesham <hamr6653@gmail.com>
2026-07-25 21:52:19 +0300

    Complete navbar and theme toggle

--- src/components/layout/Navbar/Navbar.tsx
+++ src/components/layout/Navbar/Navbar.tsx
@@ -1,4 +1,6 @@
 import { useState } from "react";
+import { useTheme } from "../../../context/ThemeContext";
+
 const Navbar = () => {
     
     const navLinkClass= "transition-colors hover:text-indigo-500 duration-200";
@@ -11,10 +13,11 @@ const Navbar = () => {
     ];
 
     const [isMenuOpen, setIsMenuOpen] = useState<boolean>(false);
-    
+    const {theme, toggleTheme} = useTheme();
+
 
     return (
-        <nav className= " px-6 h-16 bg-gray-800 text-white">
+        <nav className= "px-6 h-16 bg-white text-gray-900 dark:bg-gray-800 dark:text-white">
             <div className="container mx-auto h-full flex justify-between items-center">
                 <h1 className="text-xl font-bold text-indigo-400">React-Store</h1>
                 
@@ -27,29 +30,60 @@ const Navbar = () => {
                         </li>
                     ))}
                 </ul>
-                <input type="text" placeholder="Search..." className="hidden md:block w-36 px-2 border border-indigo-400 rounded-2xl hover:border-indigo-700 duration-200 transition-colors focus:outline-none"/>
-                <button className= "hidden md:block bg-indigo-600 text-white px-4 p-2 rounded-md hover:bg-indigo-800 transition-colors cursor-pointer">Cart</button>
+                <input type="text" placeholder="Search..." className="hidden md:block w-36 px-3 py-1.5 rounded-2xl
+                    bg-white text-gray-900 placeholder-gray-500
+                    border border-indigo-400
+                    dark:bg-gray-700 dark:text-white dark:placeholder-gray-300
+                    hover:border-indigo-700
+                    dark:hover:border-indigo-400
+                    focus:outline-none
+                    transition-colors"/>
+                <div className="hidden md:flex items-center gap-2">
+                <button className="bg-indigo-600 text-white px-4 py-2 rounded-md hover:bg-indigo-800 transition-colors cursor-pointer">
+                    Cart
+                </button>
+
+                <button
+                    onClick={toggleTheme}
+                    className="cursor-pointer text-xl"
+                >
+                    {theme === "light" ? "🌙" : "☀️"}
+                </button>
+                </div>
                 <button
                 onClick={() => setIsMenuOpen((prev) => !prev)}
-                 className="block md:hidden text-3xl cursor-pointer">
+                className="block md:hidden text-3xl cursor-pointer">
                 {isMenuOpen ? "✕" : "☰"}
                 </button>
+
+                
             </div>
 
             {isMenuOpen && (
-                <div className="md:hidden bg-gray-800 border-t border-gray-700">
+                <div className="md:hidden bg-white text-gray-900 dark:bg-gray-800 dark:text-white border-t border-gray-200 dark:border-gray-700">
+                    
+                <div className="flex items-center justify-between p-4">
+                <span>Theme</span>
+                <button
+                    onClick={toggleTheme}
+                    className="cursor-pointer text-xl"
+                >
+                    {theme === "light" ? "🌙" : "☀️"}
+                </button>
+                </div>
 
                     <ul className="flex flex-col gap-4 p-4">
                         {navLinks.map((link) =>(
                             <li key={link.name}>
                                 <a href={link.href} className={navLinkClass}
-                                 onClick={()=> setIsMenuOpen(false)}>
+                                onClick={()=> setIsMenuOpen(false)}>
                                     {link.name}
                                 </a>
                             </li>
                         ))}
                     </ul>
                 </div>
+                
             )}
         </nav>
     )
---
+++ src/context/ThemeContext.tsx
@@ -0,0 +1,48 @@
+import { createContext, useContext, useEffect, useState, type ReactNode } from "react";
+
+
+type Theme = "light" | "dark";
+
+interface ThemeContextType {
+  theme: Theme;
+  toggleTheme: () => void;
+}
+
+const ThemeContext = createContext<ThemeContextType | null>(null);
+
+interface ThemeProviderProps {
+  children: ReactNode;
+}
+
+export const ThemeProvider = ({ children }: ThemeProviderProps) => {
+  const [theme, setTheme] = useState<Theme>(() => {
+    const storedTheme = localStorage.getItem("theme");
+    return storedTheme === "dark" ? "dark" : "light";
+  });
+
+  useEffect(() => {
+    localStorage.setItem("theme", theme);
+    document.documentElement.classList.remove("light", "dark");
+    document.documentElement.classList.add(theme);
+  }, [theme]);
+
+  const toggleTheme = () => {
+    setTheme((prev) => (prev === "light" ? "dark" : "light"));
+  };
+
+  return (
+    <ThemeContext.Provider value={{ theme, toggleTheme }}>
+      {children}
+    </ThemeContext.Provider>
+  );
+};
+
+export const useTheme = () => {
+    const context = useContext(ThemeContext);
+
+    if(!context){
+        throw new Error("useTheme must be used inside ThemeProvider");
+    }
+
+    return context;
+}
