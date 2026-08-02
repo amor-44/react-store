c751fa4e882ebf915d1da7fc05afaee3e3f6428e
Amr Hesham <hamr6653@gmail.com>
2026-08-03 02:00:58 +0300

    chore: setup React Router

--- src/main.tsx
+++ src/main.tsx
@@ -4,13 +4,16 @@ import './index.css'
 import App from './App.tsx'
 import { ThemeProvider } from './context/ThemeProvider.tsx'
 import { CartProvider } from './context/CartContext.tsx'
+import {BrowserRouter} from "react-router"
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
+    <BrowserRouter>
     <ThemeProvider>
     <CartProvider>
     <App />
     </CartProvider>
     </ThemeProvider>  
+    </BrowserRouter>
   </StrictMode>,
 )
